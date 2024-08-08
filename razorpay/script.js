128331d1575864dd7d4df15b16f29a42de7cca84
Prakhar Doneria <gamesprotec@gmail.com>
2024-08-08 04:34:56 +0000

    Secure Server

---
+++ razorpay/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const uidParam = urlParams.get('uid');
+    const uidInput = document.getElementById('uid');
+
+    if (uidParam) {
+        uidInput.value = uidParam;
+        uidInput.disabled = true;
+    }
+});
+
+document.addEventListener('payment.success', async function(event) {
+    const uid = document.getElementById('uid').value;
+    const payment_id = event.detail.razorpay_payment_id;
+    const serverUrl = process.env.SERVER_URL;
+
+    try {
+        const response = await fetch(`${serverUrl}/paid?uid=${uid}&payment_id=${payment_id}`);
+        if (response.ok) {
+            const data = await response.text();
+            console.log('Server response:', data);
+        } else {
+            alert('Failed to send payment result to server');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
