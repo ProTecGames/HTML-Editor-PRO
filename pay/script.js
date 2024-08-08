90e4b82f395692d72b1993b94ade475697e2a3a2
Prakhar Doneria <gamesprotec@gmail.com>
2024-08-08 04:46:22 +0000

    Server redirect

--- pay/script.js
+++ pay/script.js
@@ -1,46 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const uidParam = urlParams.get('uid');
-  const uidInput = document.getElementById('uid');
+    const urlParams = new URLSearchParams(window.location.search);
+    const uidParam = urlParams.get('uid');
+    const uidInput = document.getElementById('uid');
 
-  if (uidParam) {
-      uidInput.value = uidParam;
-      uidInput.disabled = true;
-  }
+    if (uidParam) {
+        uidInput.value = uidParam;
+        uidInput.disabled = true;
+    }
 
-  paypal.Buttons({
-      createOrder: function(data, actions) {
-          return actions.order.create({
-              purchase_units: [{
-                  amount: {
-                      value: '6'
-                  },
-                  description: 'HTML Editor PRO -  Payment'
-              }]
-          });
-      },
-      onApprove: function(data, actions) {
-          return actions.order.capture().then(function(details) {
-              const uid = uidInput.value;
-              handleSuccess(uid);
-          });
-      },
-      onCancel: function(data) {
-          alert('Payment was canceled');
-      }
-  }).render('#paypal-button-container');
+    paypal.Buttons({
+        createOrder: function(data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: '6'
+                    },
+                    description: 'HTML Editor PRO - Payment'
+                }]
+            });
+        },
+        onApprove: function(data, actions) {
+            return actions.order.capture().then(function(details) {
+                const uid = uidInput.value;
+                handleSuccess(uid);
+            });
+        },
+        onCancel: function(data) {
+            alert('Payment was canceled');
+        }
+    }).render('#paypal-button-container');
 });
 
 async function handleSuccess(uid) {
-  try {
-      const response = await fetch(`https://pws-0h89.onrender.com/paid?uid=${uid}`);
-      if (response.ok) {
-          const data = await response.text();
-          console.log('Server response:', data);
-      } else {
-          alert('Failed to send payment result to server');
-      }
-  } catch (error) {
-      console.error('Error:', error);
-  }
+    try {
+        const response = await fetch(`https://pws-0h89.onrender.com/paid?uid=${uid}`);
+        if (response.ok) {
+            const data = await response.text();
+            console.log('Server response:', data);
+            window.location.href = '/success'; 
+        } else {
+            alert('Failed to send payment result to server');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
--- razorpay/script.js
+++ razorpay/script.js
@@ -12,13 +12,14 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('payment.success', async function(event) {
     const uid = document.getElementById('uid').value;
     const payment_id = event.detail.razorpay_payment_id;
-    const serverUrl = process.env.SERVER_URL;
+    const serverUrl = 'https://pws-0h89.onrender.com'; 
 
     try {
         const response = await fetch(`${serverUrl}/paid?uid=${uid}&payment_id=${payment_id}`);
         if (response.ok) {
             const data = await response.text();
             console.log('Server response:', data);
+            window.location.href = '/success'; 
         } else {
             alert('Failed to send payment result to server');
         }
