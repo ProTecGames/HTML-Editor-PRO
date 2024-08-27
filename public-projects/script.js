040d3992a48c9d7f5a13e553f79f91ea2e6a7477
Prakhar Doneria <gamesprotec@gmail.com>
2024-08-27 15:21:53 +0530

    Update script.js

--- public-projects/script.js
+++ public-projects/script.js
@@ -2,24 +2,26 @@ const SITE_KEY = '6LfmQTAqAAAAANucWGHhD7LDaYhH-UaP3xAdyKBG';
 
 async function fetchProjects(endpoint) {
   try {
-    await grecaptcha.ready();
-    const token = await grecaptcha.execute(SITE_KEY);
-    
-    const response = await fetch(`https://htmleditorpro.deno.dev/${endpoint}`, {
-      method: 'GET'
-    });
+    // Ensure that reCAPTCHA is ready
+    grecaptcha.ready(async () => {
+      const token = await grecaptcha.execute(SITE_KEY);
 
-    if (response.ok) {
-      const data = await response.json();
-      console.log('Fetched data:', data); // Log the data to see what’s being returned
-      if (data.status === "success" && Array.isArray(data.projects)) {
-        displayProjects(data.projects);
+      const response = await fetch(`https://htmleditorpro.deno.dev/${endpoint}`, {
+        method: 'GET'
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Fetched data:', data);
+        if (data.status === "success" && Array.isArray(data.projects)) {
+          displayProjects(data.projects);
+        } else {
+          displayError(`Invalid response structure: ${JSON.stringify(data)}`);
+        }
       } else {
-        displayError(`Invalid response structure: ${JSON.stringify(data)}`);
+        displayError(`Failed to fetch projects. Status: ${response.status}, StatusText: ${response.statusText}`);
       }
-    } else {
-      displayError(`Failed to fetch projects. Status: ${response.status}, StatusText: ${response.statusText}`);
-    }
+    });
   } catch (error) {
     displayError(`Error fetching projects: ${error.message}`);
   }
